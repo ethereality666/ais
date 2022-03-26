af2e8b92ee784a7eaba5e960773c229cddd3ae22
ethereality666 <s.kachkurkin2001@gmail.com>
2022-03-26 16:03:28 +0300

    first commit

---
+++ ais/Connector.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace ais
+{
+    public class Connector
+    {
+        MySqlConnection connection = new MySqlConnection("server=127.0.0.1;port=3306;username=aether;password=!Exc25125;database=ais");
+
+        public void OpenConnection()
+        {
+            if (connection.State == ConnectionState.Closed)
+                connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            if(connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        MySqlConnection GetConnection() => connection;
+    }
+}
